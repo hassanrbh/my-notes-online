eff667be695690e2af2c4eaae7621a04c6d014a1
João Nunes <jalnunes.2002@gmail.com>
2022-08-28 22:40:51 +0100

    Update naming in NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             </h1>
             <div className="links">
                 <Link to="/">Home</Link>
-                <Link to="/create">New Blog</Link>
+                <Link to="/create">Add Notes</Link>
             </div>
         </nav>
     );
